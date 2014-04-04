11b5bf16588a416d6912bd486650915569b95815
guixii <gp.vassal@gmail.com>
2014-04-04 10:04:03 +0200

    shmFinal

--- sharemem/sharemem.h
+++ sharemem/sharemem.h
@@ -1,12 +1,5 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <signal.h>
-
-#include <sys/types.h>
-#include <sys/ipc.h>
 #include <sys/shm.h>
 
-#include <fcntl.h>
+#define CLE  1912
